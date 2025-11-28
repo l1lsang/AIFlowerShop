c3f5bb4715b047431f2538f5d71f4f1cbfc9becb
gmjang <jkm0831123@gmail.com>
2025-11-28 10:23:31 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,22 +3,11 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter, Navigate } from "react-router-dom";
-
-function RootRouter() {
-  return (
-    <BrowserRouter>
-      {/* 첫 접속 시 무조건 /login으로 이동 */}
-      <Navigate to="/login" replace />
-      <App />
-    </BrowserRouter>
-  );
-}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RootRouter />
+    <App />
   </React.StrictMode>
 );
 
